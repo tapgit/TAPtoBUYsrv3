38aa0fa49df4cfbe9c54fd469817cddf04812c27
Kidany <kidobv@gmail.com>
2013-12-11 21:28:27 -0400

    manager

--- app/man/Manager.java
+++ app/man/Manager.java
@@ -18,16 +18,16 @@ public class Manager {
 	public static String pass = "postgres";
 	
 	//Image Dir (local)
-	public static String imagesDir = "/home/cok0/git/TAPtoBUYsrv3/images/";
-	public static String imagesScaledDir = "/home/cok0/git/TAPtoBUYsrv3/images/scaled/";
-	//public static String imagesDir = "C:\\Users\\Kidany\\git\\TAPtoBUYsrv3\\images\\scaled\\";
-	//public static String imagesDir = "C:\\Users\\Kidany\\git\\TAPtoBUYsrv3\\images\\scaled\\";
+	//public static String imagesDir = "/home/cok0/git/TAPtoBUYsrv3/images/";
+	//public static String imagesScaledDir = "/home/cok0/git/TAPtoBUYsrv3/images/scaled/";
+	  public static String imagesDir = "C:\\Users\\Kidany\\git\\TAPtoBUYsrv3\\images\\";
+	  public static String imagesScaledDir = "C:\\Users\\Kidany\\git\\TAPtoBUYsrv3\\images\\scaled\\";
 	
 	//Image localhost url to send to the client (Android)
-	public static String andrImgDir = "http://10.0.2.2:9000/images/";
-	public static String andrScaledImgDir = "http://10.0.2.2:9000/images/scaled/";
+	//public static String andrImgDir = "http://10.0.2.2:9000/images/";
+	//public static String andrScaledImgDir = "http://10.0.2.2:9000/images/scaled/";
 	//Image localhost url to send to the client (Android)
-	//public static String andrImgDir = "http://24.54.234.201:9000/images/";
-	//public static String andrScaledImgDir = "http://24.54.234.201:9000/images/scaled/";
+	public static String andrImgDir = "http://24.54.234.201:9000/images/";
+	public static String andrScaledImgDir = "http://24.54.234.201:9000/images/scaled/";
 	
 }
